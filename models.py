251fdfdeac06b8bc331c37b9994762819b5ead97
Severus Melvin <melvinmulyono02@gmail.com>
2026-05-31 21:06:57 +0700

    part eig

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ from database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users" 
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
@@ -21,7 +21,7 @@ class User(Base):
         default=None,
     )
 
-    posts: Mapped[list[Post]] = relationship(back_populates="author")
+    posts: Mapped[list[Post]] = relationship(back_populates="author", cascade="all, delete-orphan")
 
     @property
     def image_path(self) -> str:
